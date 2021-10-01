cc480a1307983693b9d9894ff451f231a71e64fc
Joshua Alvarez <joshuaialvarez06@gmail.com>
2021-10-01 11:03:36 -0700

    change to router

--- src/urls/urls.router.js
+++ src/urls/urls.router.js
@@ -1,4 +1,4 @@
-const router = require("express").Router({ mergeParams: true });
+const router = require("express").Router();
 const controller = require("./urls.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 const usesRouter = require("../uses/uses.router");
